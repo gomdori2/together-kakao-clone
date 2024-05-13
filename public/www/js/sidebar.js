4cda202c76ca48226d845c8e8d6d5eedaff84943
gomdori2 <dbstjdghks21@naver.com>
2024-05-13 18:14:25 +0900

    feature : sidebar 수정 중

--- public/www/js/sidebar.js
+++ public/www/js/sidebar.js
@@ -1,21 +1,20 @@
 window.addEventListener("load", function () {
+  console.log("asdasdasd");
   // 애니메이션으로 바꿔야함. sidebar 안쪽 구조 좀 건드려야함.
-  window.addEventListener("load", function () {
-    const sidebar = {
-      sidebarWrap: document.querySelector(".sidebar-wrap"),
-      icoToggle: document.querySelector(".ico-toggle"),
-      sidebarWrapInner: document.querySelector(".sidebar-wrap-inner"),
-    };
-    const { sidebarWrap, icoToggle, sidebarWrapInner } = sidebar;
-    sidebarWrap.addEventListener("click", (e) => {
-      if (e.target.className === "sidebar-wrap") {
-        sidebarWrap.style.display = "none";
-        sidebarWrapInner.classList.remove("sidebar-show");
-      }
-    });
-    icoToggle.addEventListener("click", () => {
-      sidebarWrap.style.display = "block";
-      sidebarWrapInner.classList.add("sidebar-show");
-    });
+  const sidebar = {
+    sidebarWrap: document.querySelector(".sidebar-wrap"),
+    icoToggle: document.querySelector(".ico-toggle"),
+    sidebarWrapInner: document.querySelector(".sidebar-wrap-inner"),
+  };
+  const { sidebarWrap, icoToggle, sidebarWrapInner } = sidebar;
+  sidebarWrap.addEventListener("click", (e) => {
+    console.log("aaa");
+    if (e.target.className === "sidebar-wrap") {
+      sidebarWrap.classList.remove("sidebar-show");
+    }
+  });
+  icoToggle.addEventListener("click", () => {
+    console.log("aaa");
+    sidebarWrap.classList.add("sidebar-show");
   });
 });
